8e3a5b8cfb91d55b0fb88f4d6628a6dc21633b8c
Tom Hu <tomhu1096+gpg@gmail.com>
2023-04-20 18:13:18 -0700

    feat: enable branch coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,5 @@ set -x
 # Check README.md is up to date
 python ./scripts/docs.py verify-readme
 export PYTHONPATH=./docs_src
-coverage run -m pytest tests ${@}
+coverage run --branch -m pytest tests "${@}"
+
